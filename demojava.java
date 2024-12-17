06f67cf408992c7f7abb85cb6739f0b05717057a
Premdevi Kumawat <61683230+Premdevi9091@users.noreply.github.com>
2024-12-17 21:17:44 +0530

    Update demojava.java

--- demojava.java
+++ demojava.java
@@ -3,6 +3,6 @@ class demojava{
     public static void main(String[] args) {
         System.out.println("Hello WOrld");
         System.out.println("Hello WOrld Added from git");
-        System.out.println("Hello WOrld Added from git test1 branch");
+        System.out.println("Hello WOrld Added from git test1 branch ");
     }
 }
