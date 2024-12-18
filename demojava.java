77fe2ba6642f8b5eb9d1cc326b683791656cf1ff
premdevi9091 <premdevi9091@gmail.com>
2024-12-18 21:18:22 +0530

    checking hard

--- demojava.java
+++ demojava.java
@@ -3,5 +3,6 @@ class demojava{
     public static void main(String[] args) {
         System.out.println("Hello WOrld");
         System.out.println("Hello WOrld Added from git");
+        System.out.println("to check head and hard");
     }
 }
